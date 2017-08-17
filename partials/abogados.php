ff0458fe637d106abf87e58c67d855957497cdd6
Sergio <sferrando.92@gmail.com>
2017-08-17 13:50:04 +0200

    create new partial abogados

---
+++ partials/abogados.php
@@ -0,0 +1,43 @@
+<div class="container">
+  <div class="row lawyer">
+        <div class="col-xs-12 col-md-2" >
+            <img class="img-circle img-responsive img-center" style="width: 150px;  " src="images/logo_min.png">
+        </div>
+
+        <div class="col-xs-12 col-md-10">
+            <h2 style="margin: .5em;">Alberto Guarch Brisa</h2>
+            <p>Mew. Vommit food and eat it again purrrrrr yet leave hair everywhere, or hide from vacuum cleaner. Where is my slave? I'm getting hungry cat mojo destroy the blinds hack up furballs and sit on the laptop, for meowing chowing and wowing swat at dog. Meow loudly just to annoy owners woops poop hanging from butt must get rid run run around house drag poop on floor maybe it comes off woops left brown marks on floor human slave clean lick butt now.</p>
+            <div class="contact-data">
+                <p><a href="mailto:alberto@guarchabogados.com">alberto@guarchabogados.com</a></p>
+                <p><a href="http://legadoo.com/abogado_valencia/alberto-guarch-brisa/">Mas información</a></p>
+            </div>
+        </div>
+    </div>
+    <div class="clearfix"></div>
+    <div class="row lawyer">
+        <div class="col-xs-12 col-md-2" >
+            <img class="img-circle img-responsive img-center" style="width: 150px;" src="images/logo_min.png">
+        </div>
+        <div class="col-xs-12 col-md-10">
+            <h2 style="margin: .5em;">Ricardo Guarch Bonora</h2>
+            <p>Mew. Vommit food and eat it again purrrrrr yet leave hair everywhere, or hide from vacuum cleaner. Where is my slave? I'm getting hungry cat mojo destroy the blinds hack up furballs and sit on the laptop, for meowing chowing and wowing swat at dog. Meow loudly just to annoy owners woops poop hanging from butt must get rid run run around house drag poop on floor maybe it comes off woops left brown marks on floor human slave clean lick butt now.</p>
+            <div class="contact-data">
+                <p ><a href="mailto:alberto@guarchabogados.com">ricardo@guarchabogados.com</a></p>
+            </div>
+        </div>
+    </div>
+    <div class="clearfix"></div>
+    <div class="row lawyer">
+        <div class="col-xs-12 col-md-2" >
+            <img class="img-circle img-responsive img-center" style="width: 150px;" src="images/logo_min.png">
+        </div>
+
+        <div class="col-xs-12 col-md-10">
+            <h2 style="margin: .5em;">Alfredo Solaz Andres</h2>
+            <p>Mew. Vommit food and eat it again purrrrrr yet leave hair everywhere, or hide from vacuum cleaner. Where is my slave? I'm getting hungry cat mojo destroy the blinds hack up furballs and sit on the laptop, for meowing chowing and wowing swat at dog. Meow loudly just to annoy owners woops poop hanging from butt must get rid run run around house drag poop on floor maybe it comes off woops left brown marks on floor human slave clean lick butt now.</p>
+            <div class="contact-data">
+                <p ><a href="mailto:alfredo@guarchabogados.com">alfredo@guarchabogados.com</a></p>
+            </div>
+        </div>
+    </div>
+</div>
