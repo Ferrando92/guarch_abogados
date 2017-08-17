ce3f79834543925be38025ca96c6b8d142285d4e
Sergio <sferrando.92@gmail.com>
2017-08-17 13:50:27 +0200

    DRY and change texts

--- divorcio-valencia.php
+++ divorcio-valencia.php
@@ -30,60 +30,8 @@
     <script src="js/jquery.chocolat.js"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
     <script async="" src="https://www.google-analytics.com/analytics.js"></script>
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-76462931-1', 'auto');
-      ga('send', 'pageview');
-
-    </script>
-    <script type="application/ld+json">
-      {
-        "@context": "http://schema.org/",
-        "@type": "LegalService",
-        "@id":"http://www.guarchabogados.com/",
-        "name": "Guarch Abogados",
-        "url": "http://www.guarchabogados.com/",
-        "telephone" : "+34 961 047 508",
-        "email": "contacto@guarchabogados.com",
-        "logo": "http://www.guarchabogados.com/images/logo-min.png",
-        "address": {
-          "@type": "PostalAddress",
-          "addressCountry":"Spain",
-          "addressLocality":"Valencia",
-          "addressRegion":"Valencia",
-          "availableLanguage":"Spanish, English",
-          "contactOption":"email, telephone",
-          "email":"contacto@guarchabogados.com",
-          "name":"Guarch Abogados",
-          "postalCode":"46001",
-          "streetAddress":"Calle Garrigues nº17, 1er Piso, 2ª Puerta",
-          "telephone":"+34 961 047 508",
-          "url":"http://www.guarchabogados.com"
-        },
-        "geo":{
-        "@type":"GeoCoordinates",
-        "latitude":39.4688062,
-        "longitude":-0.374473
-        },
-        "alternateName":"Guarch Abogados Valencia",
-        "alumni": [
-        {
-        "@type": "Person",
-        "name": "Ricardo Guarch"
-        },
-        {
-        "@type": "Person",
-        "name": "Alberto Guarch"
-        }
-        ],
-        "brand":"Guarch abogados",
-        "image": "http://www.guarchabogados.com/images/logo.png"
-      }
-    </script>
+    <?php require($DOCUMENT_ROOT . "partials/analytics.php"); ?>
+    <?php require($DOCUMENT_ROOT . "partials/jsonld.php"); ?>
   </head>
   <body>
     <div style="position:fixed;top:0px;left:0px;width:0;height:0;" id="scrollzipPoint"></div>
@@ -171,68 +119,7 @@
 
         <?php require($DOCUMENT_ROOT . "partials/contacto.php"); ?>
 
-        <footer>
-          <div class="row">
-            <div class="col-lg-12">
-              <div class="footer footer-info"">
-                <div class="col-xs-12  col-md-4 footer-info-grid links">
-                  <h4>ENCUÉNTRANOS</h4>
-                  <div class="contact-bottom">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3079.922478917067!2d-0.381466084632598!3d39.471079979486206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd604f4c1c85b2e1%3A0x26ea0be400a35866!2sGuarch+Abogados+-+Valencia!5e0!3m2!1ses!2ses!4v1501970684973"  frameborder="0" style="border:0" allowfullscreen></iframe>
-                  </div>
-                </div>
-                <div class="col-xs-12 col-md-4 footer-info-grid address">
-                  <h4>DIRECCIÓN</h4>
-                  <address itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
-                    <ul>
-                      <li itemprop="streetAddress">
-                        <span itemprop="streetAddress"> Calle Garrigues nº17
-                        <br/> 1er Piso, 2ª Puerta </span>
-                      </li>
-                      <li>
-                        <span itemprop="postalCode"> 46001</span>
-                        <span itemprop="addressLocality">Valencia</span>,
-                        <span itemprop="addressCountry">España</span>
-                      </li>
-                      <li>
-                        Teléfono: <span itemprop="telephone" >+34 961 047 508</span>
-                      </li>
-                    </ul>
-                  </address>
-                </div>
-                <div class="col-xs-12 col-md-4  footer-info-grid newsletter">
-                  <h4>CONTÁCTANOS</h4>
-                  <p>Puedes enviarnos un correo a nuestro correo electronico: </p>
-                  <li>
-                    Email: <a class="mail" href="mailto:alberto@guarchabogados.com"><span itemprop="email"> contacto@guarchabogados.com </span></a>
-                  </li>
-                  <li>
-                    <a href="tel:+34961047508"><img class="contact-icon" src="images/icons/phone-icon.png" alt ='guarch abogados teléfono'></a>
-                    <a href="https://www.facebook.com/guarchabogados/"><img class="contact-icon" src="images/icons/fb-icon.png" alt ='guarch abogados facebook'></a>
-                    <a href="https://www.google.es/maps/place/Guarch+Abogados+-+Valencia/@39.47108,-0.3814661,17z/data=!3m1!4b1!4m5!3m4!1s0xd604f4c1c85b2e1:0x26ea0be400a35866!8m2!3d39.47108!4d-0.3792774"><img class="contact-icon" src="images/icons/map-icon.png" alt ='valencia'></a>
-                  </li>
-                </div>
-                <div class="SeoHiddenData">
-                  <span itemprop="url">http://www.guarchabogados.com</span>
-                  <span itemprop="alternateName">Guarch abogados valencia</span>
-                  <span itemprop="brand">Guarch abogados</span>
-                  <span itemprop="alumni" itemscope="" itemtype="http://schema.org/Person">
-                    <span itemprop="name">Ricardo Guarch</span>
-                  </span>
-                  <span itemprop="alumni" itemscope="" itemtype="http://schema.org/Person">
-                    <span itemprop="name">Alberto Guarch</span>
-                  </span>
-                </div>
-                <div class="col-md-12 col-sm-12 footer-info-grid">
-                  <p style="text-align: center"> © 2017
-                    <span itemprop="url">Guarchabogados.com</span>, Todos los derechos reservados.
-                  </p>
-                </div>
-                <div class="clearfix"></div>
-              </div>
-            </div>
-          </div>
-        </footer>
+        <?php require($DOCUMENT_ROOT . "partials/footer.php"); ?>
     </div>
   </body>
 </html>
---
+++ partials/jsonld.php
@@ -0,0 +1,44 @@
+<script type="application/ld+json">
+  {
+    "@context": "http://schema.org/",
+    "@type": "LegalService",
+    "@id":"http://www.guarchabogados.com/",
+    "name": "Guarch Abogados",
+    "url": "http://www.guarchabogados.com/",
+    "telephone" : "+34 961 047 508",
+    "email": "contacto@guarchabogados.com",
+    "logo": "http://www.guarchabogados.com/images/logo-min.png",
+    "address": {
+      "@type": "PostalAddress",
+      "addressCountry":"Spain",
+      "addressLocality":"Valencia",
+      "addressRegion":"Valencia",
+      "availableLanguage":"Spanish, English",
+      "contactOption":"email, telephone",
+      "email":"contacto@guarchabogados.com",
+      "name":"Guarch Abogados",
+      "postalCode":"46001",
+      "streetAddress":"Calle Garrigues nº17, 1er Piso, 2ª Puerta",
+      "telephone":"+34 961 047 508",
+      "url":"http://www.guarchabogados.com"
+    },
+    "geo":{
+    "@type":"GeoCoordinates",
+    "latitude":39.4688062,
+    "longitude":-0.374473
+    },
+    "alternateName":"Guarch Abogados Valencia",
+    "alumni": [
+    {
+    "@type": "Person",
+    "name": "Ricardo Guarch"
+    },
+    {
+    "@type": "Person",
+    "name": "Alberto Guarch"
+    }
+    ],
+    "brand":"Guarch abogados",
+    "image": "http://www.guarchabogados.com/images/logo.png"
+  }
+</script>
